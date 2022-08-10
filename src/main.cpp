44d480fe73c8cda172fcf9c65bb640a70e02a5dd
Albio-uni <alberto.carli_01@studenti.univr.it>
2022-08-10 12:46:16 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 
 #define cimg_display 0
 #define cimg_use_png 1
-#define cimg_use_tiff 1
+#define cimg_use_tiff
 #define cimg_use_jpeg 1
 
 #define cimg_OS 1
@@ -18,22 +18,6 @@
 
 using namespace std;
 using namespace cimg_library;
-
-/*
-template <typename T>
-void printMat(NDArray<T> input)
-{
-    for (uint16_t i = 0; i < input.size(); i++)
-    {
-        
-        if (i % input.getShape()[0] == 0)
-            cout << "\n";
-        
-        cout << input[i] << "\t";
-    }
-    
-    cout << "\n";
-}*/
 
 int main() {
 
